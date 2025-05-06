6c709798621bdd978d66eb4c7e2f4994602f9ecb
Thierno Abdoulaye DIALLO <thierno_abdoulaye.diallo2@etu.uca.fr>
2025-05-06 17:44:40 +0200

    Ajout .gitignore et suppression fichiers compilés du suivi

--- airflow/dags/scripts/fetch_data.py
+++ airflow/dags/scripts/fetch_data.py
@@ -1,5 +1,3 @@
-# script qui va appeler l'API JCDecaux
-# pour récupérer les stations de vélos
 from matplotlib.pylab import f
 import pandas as pd
 import requests
@@ -11,15 +9,15 @@ from datetime import datetime, timedelta
 
 load_dotenv()
 
-# Récupération de la clé API depuis airflow 
+
 API_KEY = Variable.get("API_KEY")
 CONTRACT_NAME = os.getenv("CONTRACT_NAME")
 # Récupération de l'URL de l'API JCDecaux
 
 API_URL = f" https://api.jcdecaux.com/vls/v1/stations?contract={CONTRACT_NAME}&apiKey={API_KEY}"
-#definition de la fonction fetxh_data
+
 def fetch_data() : 
-    # Appel de l'API JCDecaux
+    
     response = requests.get(API_URL)
 
     if response.status_code != 200:
@@ -33,10 +31,9 @@ def fetch_data() :
     #ajout d'un timestamp à chaque ligne 
     df['timestamp'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-    #creer le dossier data s'il n'existe pas
     if not os.path.exists("data"):
         os.makedirs("data")
-    # nom du fichier de sortie
+    
     filename = f"data/stations_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.csv"
 
     df.to_csv(filename, index=False)
--- airflow/dags/velib_fetch_dag.py
+++ airflow/dags/velib_fetch_dag.py
@@ -23,5 +23,3 @@ with DAG(
         
        
     )
-
-# fetch_data
